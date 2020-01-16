c922e2fb5ff99d1170f4ef460d01261eb772bc08
GlobalServicesAndSolutions <59004123+GlobalServicesAndSolutions@users.noreply.github.com>
2020-01-16 22:25:35 +0500

    Some changes

--- src/Modules/CandidateDomain/CandidatesList/CandidateListHeader.js
+++ src/Modules/CandidateDomain/CandidatesList/CandidateListHeader.js
@@ -4,12 +4,48 @@ const CandidateListHeader = () => {
   return (
     <div className="profile-intro card shadow  border-0 text-left">
 
-      <div >
         <div className="card-image">
         <h4>Operations Manager</h4>
-        </div>
-      </div>
-      <div className="pi-content">
+        <table className='customers' border='1'> 
+          <tr>
+            <th>
+              Ready To Apply<br/>
+              0
+            </th>
+            <th>
+              InterView<br/>
+              0
+            </th>
+            <th>
+              New Application<br/>
+              0
+            </th>
+            <th>
+              Short List<br/>
+              0
+            </th>
+            <th>
+              Background Check<br/>
+              0
+            </th>
+            <th>
+              Assesments<br/>
+              0
+            </th>
+            <th>
+              Offers<br/>
+              0
+            </th>
+            <th>
+              Ready For Hire<br/>
+              0
+            </th>
+            <th>
+              Hired<br/>
+              0
+            </th>
+          </tr>
+        </table>
       </div>
     </div>
   )
